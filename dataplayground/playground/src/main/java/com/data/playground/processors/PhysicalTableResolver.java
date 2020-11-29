4a2cc66387ca333d69c1ced5abea10bead2773ef
Palani <palaniappa@gmail.com>
2020-11-29 14:02:34 +0530

    Changes to solve the alias resolution

--- dataplayground/playground/src/main/java/com/data/playground/processors/PhysicalTableResolver.java
+++ dataplayground/playground/src/main/java/com/data/playground/processors/PhysicalTableResolver.java
@@ -3,11 +3,14 @@ package com.data.playground.processors;
 import com.data.playground.exception.PlaygroundException;
 import com.data.playground.repositories.entity.Catalog;
 import com.data.playground.repositories.entity.DPTable;
+import com.data.playground.util.CommonUtil;
+import net.sf.jsqlparser.expression.Alias;
 import net.sf.jsqlparser.schema.Table;
 import net.sf.jsqlparser.statement.select.Select;
 import net.sf.jsqlparser.util.TablesNamesFinder;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -15,25 +18,31 @@ public class PhysicalTableResolver extends TablesNamesFinder {
 
     private Map<String,DPTable> tableMap;
     private Map<String,Catalog> catalogMap;
-    private List<String> tablesFailedResolution;
+    private Map<String, String> tablesFailedResolution;
+    private Map<String, String> tableAliases;
 
     public PhysicalTableResolver(Map<String, DPTable> tableMap, Map<String, Catalog> catalogMap) {
         this.catalogMap = catalogMap;
         this.tableMap = tableMap;
-        this.tablesFailedResolution = new ArrayList<>();
+        this.tablesFailedResolution = new HashMap<>();
+        this.tableAliases = new HashMap<>();
     }
 
     public boolean getProcessedStatement(Select statement) {
         this.init(true);
-        this.tablesFailedResolution = new ArrayList<>();
+        this.tablesFailedResolution = new HashMap<>();
+        this.tableAliases = new HashMap<>();
         statement.accept(this);
+        this.tableAliases.values().forEach( a -> {
+            this.tablesFailedResolution.remove(a);
+        });
         return this.tablesFailedResolution.size() == 0;
     }
 
     public String getError(){
         StringBuilder tableNames = new StringBuilder();
         boolean first = true;
-        for(String table : this.tablesFailedResolution) {
+        for(String table : this.tablesFailedResolution.values()) {
             if(!first) {
                 tableNames.append(", ");
             }
@@ -58,9 +67,14 @@ public class PhysicalTableResolver extends TablesNamesFinder {
                 tableName.setName(String.format("%s.%s.%s",catalog.getId(), catalog.getDatabaseName(), table.getId()));
                 failed = false;
             }
+
+            Alias alias = tableName.getAlias();
+            if(alias != null && !CommonUtil.isEmpty(alias.getName())) {
+                this.tableAliases.put(alias.getName(), alias.getName());
+            }
         }
         if(failed) {
-            tablesFailedResolution.add(tableName.getName());
+            tablesFailedResolution.put(tableName.getName(),tableName.getName());
         }
 
     }
